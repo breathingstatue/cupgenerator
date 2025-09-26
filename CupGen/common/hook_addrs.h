a5a25b70dc0297fd6e68afca4bfda42ef4a72251
breathingstatue <149799034+breathingstatue@users.noreply.github.com>
2025-09-26 18:25:13 +0300

    Add files via upload

---
+++ CupGen/common/hook_addrs.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+// common/hook_addrs.h
+// Shared between RVGLCupOpponents (mod) and RvglSigHelper (scanner)
+
+#include <cstdint>
+
+struct HookAddrs {
+    uint32_t rva_LoadCars{};
+    uint32_t rva_CupParse{};
+    uint32_t rva_BuildGrid{};
+    uint32_t rva_CarTablePtr{};
+    uint32_t rva_CarCount{};
+    uint32_t rva_AISlot0{};
+    uint32_t rva_ActiveCupPtr{};
+    uint32_t rva_PlayersBase{};
+    uint32_t rva_PlayersCount{};
+    uint32_t rva_RaceResults{};
+    uint32_t rva_CupFinalize{};
+    uint32_t rva_OppSlotIndex[16]{};
+};

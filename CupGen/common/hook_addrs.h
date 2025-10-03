be745778566c40817641fc8a837356811a6ac6f7
breathingstatue <149799034+breathingstatue@users.noreply.github.com>
2025-10-03 20:12:56 +0300

    v.1.3

--- CupGen/common/hook_addrs.h
+++ CupGen/common/hook_addrs.h
@@ -5,16 +5,24 @@
 #include <cstdint>
 
 struct HookAddrs {
-    uint32_t rva_LoadCars{};
-    uint32_t rva_CupParse{};
-    uint32_t rva_BuildGrid{};
-    uint32_t rva_CarTablePtr{};
-    uint32_t rva_CarCount{};
-    uint32_t rva_AISlot0{};
-    uint32_t rva_ActiveCupPtr{};
-    uint32_t rva_PlayersBase{};
-    uint32_t rva_PlayersCount{};
-    uint32_t rva_RaceResults{};
-    uint32_t rva_CupFinalize{};
+    uint32_t rva_LoadCars = 0;
+    uint32_t rva_CupParse = 0;
+    uint32_t rva_BuildGrid = 0;
+    uint32_t rva_CarTablePtr = 0;
+    uint32_t rva_CarCount = 0;
+    uint32_t rva_AISlot0 = 0;
+    uint32_t rva_ActiveCupPtr = 0;
+    uint32_t rva_PlayersBase = 0;
+    uint32_t rva_PlayersCount = 0;
+    uint32_t rva_RaceResults = 0;
+    uint32_t rva_CupFinalize = 0;
     uint32_t rva_OppSlotIndex[16]{};
+
+    // already added by you:
+    uint32_t rva_MenuState = 0;       // [*MenuState + 0x04] = selected index
+    uint32_t rva_BuiltinCupsBase = 0; // builtin cups table base
+    uint32_t rva_CustomCupsList = 0;  // ptr to custom cups array
+
+    // add these two for the frontend handle:
+    uint32_t rva_FrontendInit = 0;    // the frontend/menu bootstrap function (your target)
 };
